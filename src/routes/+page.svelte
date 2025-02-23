c9d8212dde7dad1a31b0fb46c2989e0b8ba6ef7b
Andrew Baughman <andrewbaughman@icloud.com>
2025-02-22 21:27:47 -0500

    Change page styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,18 @@
     <title>DruLand</title>    
 </svelte:head>
 
-<span>THIS<br/> IS<br/> MY<br/> WEBSITE</span>
+<p>THIS</p>
+<p>IS</p>
+<p>MY</p>
+<p>WEBSITE</p>
 
 <style>
-    span {
+    p {
         font-size: 12em;
         font-weight: bold;
+        margin-block-start: 0px;
+        margin-block-end: 0px;
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
     }
 </style>
