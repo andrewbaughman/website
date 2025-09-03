59ac2233fa74c75f47c15cdfb690b08048139dc1
Andrew Baughman <abaughman@brotherhoodmutual.com>
2025-09-03 07:40:07 -0400

    Add projects, add Today is project

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Projects</h1>
+<ul>
+    <li><a href="/todayis">Today is...</a></li>
+</ul>
---
+++ src/routes/projects/todayis/+page.svelte
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Today is ${weekday}.</title>
+    </head>
+    <body style="background-color: black">
+        <h1
+            style="color: white; font-size: 10rem; text-align: center"
+            id="weekday"
+        />
+
+        <script>
+            const weekday = new Date().toLocaleDateString("en-US", {
+                weekday: "long",
+            });
+            document.getElementById("weekday").textContent =
+                `Today is ${weekday}.`;
+            document.title = `Today is ${weekday}.`;
+        </script>
+    </body>
+</html>
