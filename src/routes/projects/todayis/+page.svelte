544e3bbffc30231eb06d63b9e8a7f435094f319c
Andrew Baughman <abaughman@brotherhoodmutual.com>
2025-09-03 08:18:03 -0400

    Address linter errors

--- src/routes/projects/todayis/+page.svelte
+++ src/routes/projects/todayis/+page.svelte
@@ -1,21 +1,16 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Today is ${weekday}.</title>
-    </head>
-    <body style="background-color: black">
-        <h1
-            style="color: white; font-size: 10rem; text-align: center"
-            id="weekday"
-        />
-
-        <script>
-            const weekday = new Date().toLocaleDateString("en-US", {
-                weekday: "long",
-            });
-            document.getElementById("weekday").textContent =
-                `Today is ${weekday}.`;
-            document.title = `Today is ${weekday}.`;
-        </script>
-    </body>
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<script>
+			const weekday = new Date().toLocaleDateString('en-US', {
+				weekday: 'long'
+			});
+			document.getElementById('weekday').textContent = `Today is ${weekday}.`;
+			document.title = `Today is ${weekday}.`;
+		</script>
+		<title></title>
+	</head>
+	<body style="background-color: black">
+		<h1>style="color: white; font-size: 10rem; text-align: center" id="weekday"></h1>
+	</body>
 </html>
