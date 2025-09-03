99a59ea6439f60ea1f11330664a103564e7cb999
Andrew Baughman <abaughman@brotherhoodmutual.com>
2025-09-03 08:28:12 -0400

    Remove link to projects

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,27 +1,27 @@
 <header>
-    <nav>
-        <a href="/">home</a>
-        <a href="/blog">blog</a>
-        <a href="/projects">projects</a>
-    </nav>
+	<nav>
+		<a href="/">home</a>
+		<a href="/blog">blog</a>
+		<!-- <a href="/projects">projects</a> -->
+	</nav>
 </header>
 
 <style>
-    header {
-        padding: 1rem;
+	header {
+		padding: 1rem;
 		background: #f8f9fa;
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
-    }
+	}
 
-    nav a {
-        margin: 0 1rem;
-        text-decoration: none;
-        color: #007bff;
-    }
+	nav a {
+		margin: 0 1rem;
+		text-decoration: none;
+		color: #007bff;
+	}
 
-    nav a:hover {
-        text-decoration: underline;
-    }
+	nav a:hover {
+		text-decoration: underline;
+	}
 </style>
