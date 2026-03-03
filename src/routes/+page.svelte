cd1482e94017dc0754c40e87d89b5596f891dff0
Andrew Baughman <abaughman@brotherhoodmutual.com>
2026-03-02 21:25:41 -0500

    Revert "Change page styling"
    This reverts commit c9d8212dde7dad1a31b0fb46c2989e0b8ba6ef7b.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,11 @@
     <title>DruLand</title>    
 </svelte:head>
 
-<p>THIS</p>
-<p>IS</p>
-<p>MY</p>
-<p>WEBSITE</p>
+<span>THIS<br/> IS<br/> MY<br/> WEBSITE</span>
 
 <style>
-    p {
+    span {
         font-size: 12em;
         font-weight: bold;
-        margin-block-start: 0px;
-        margin-block-end: 0px;
-        font-family: Arial, Helvetica, sans-serif;
-        text-align: center;
     }
 </style>
